8a4e584a18dabc7b3cf3e4f665f4d0bf20afb3ec
HolyAntimony <maya@alyssa.ws>
2022-01-16 05:38:44 -0500

    Add HTTP Proxy

---
+++ client.lua
@@ -0,0 +1,147 @@
+local device = require("devices")
+local socket = require("socket")
+--local json = require("json")
+local ld = require("LibDeflate")
+local base64 = require("base64")
+local time = require("posix.time")
+
+local robot = device:find("robot")
+local inv = device:find("inventory_operations")
+
+function getData()
+    local fileData = ""
+    local ready = false
+    while not ready do
+        time.nanosleep({tv_sec=0,tv_nsec=150*1000*1000})
+        while inv:takeFrom(0, 1, "front") == 0 do end
+        local item = robot:getStackInSlot(0)
+        if item["tag"] ~= nil and item["tag"]["display"] ~= nil and item["tag"]["display"]["Name"] ~= nil then
+            local itemNameJson = item["tag"]["display"]["Name"]
+            local itemName = string.sub(itemNameJson, 10, -3)    
+            if itemName == "SENDING" or itemName == "DONE" then
+                ready = true
+            end
+        end
+        inv:dropInto(0, 1, "front")
+    end
+
+    local slot = 1
+    local chestSize = 27
+    local done = false
+
+    while not done do
+        if inv:takeFrom(slot, 1, "front") == 0 then
+            while inv:takeFrom(0, 1, "front") == 0 do end
+            local item = robot:getStackInSlot(0)
+            local itemNameJson = item["tag"]["display"]["Name"]
+            inv:dropInto(0, 1, "front")
+            --local itemName = json.decode(itemNameJson)["text"]
+            local itemName = string.sub(itemNameJson, 10, -3)    
+            if itemName == "DONE" then
+                while inv:takeFrom(0, 1, "front") == 0 do end
+                print("Request finished!")
+                inv:drop(1, "up")
+                done = true
+                break
+            else
+                if itemName ~= "SENDING" then
+                    print("Got unusual item name " .. itemName)
+                end
+                while inv:takeFrom(slot, 1, "front") == 0 do end
+            end
+        end
+        local item = robot:getStackInSlot(0)
+        local itemNameJson = item["tag"]["display"]["Name"]
+        --local itemName = json.decode(itemNameJson)["text"]
+        local itemName = string.sub(itemNameJson, 10, -3)
+        inv:drop(1, "up")
+        local data = base64.decode(itemName)
+        fileData = fileData .. data
+
+        slot = slot + 1
+        if slot >= chestSize then
+            slot = 1
+        end
+    end
+
+    local output = ld:DecompressZlib(fileData)
+    return output
+    --[[local file = io.open("output.txt", "wb")
+    file:write(output)
+    io.flush()
+    file:close()]]
+end
+
+local prev=16
+function sendNibble(value)
+    while inv:takeFrom(prev, 1, "front") == 0 do end
+    if prev ~= value then
+        while inv:dropInto(prev, 1, "front") == 0 do end
+        while inv:takeFrom(value, 1, "front") == 0 do end
+    end
+    -- Drop into data line slot
+    while inv:dropInto(17, 1, "front") == 0 do end
+    prev = value
+end
+
+function requestURL(url)
+    robot:turn("right")
+    for i = 1, #url do
+        local char = string.byte(url:sub(i,i))
+        local upper = char >> 4
+        local lower = (char & 0x0F)
+        sendNibble(upper)
+        sendNibble(lower)
+    end
+    sendNibble(16)
+    print("Turning left")
+    time.nanosleep({tv_sec=1,tv_nsec=50*1000*1000})
+    robot:turn("left")
+    print("Turned left")
+    time.nanosleep({tv_sec=1,tv_nsec=50*1000*1000})
+    print("Stuffs")
+end
+
+local server = socket.tcp()
+server:bind("*", 80)
+server:listen(5)
+local ip, port = server:getsockname()
+while port ~= "80" do
+    server:close()
+    server = socket.tcp()
+    server:bind("*", 80)
+    server:listen(5)
+    time.nanosleep({tv_sec=0,tv_nsec=150*1000*1000})
+    ip, port = server:getsockname()
+end
+print("Listening on IP="..ip..", PORT="..port.."...")
+
+while true do
+    local client = server:accept()
+    if client then
+        print("Client connection!")
+        local data = ""
+        local line, err = client:receive()
+        line = string.sub(line, 5)
+        local path = string.sub(line, 1, string.find(line, " ")-1)
+        local host
+        while line ~= "" do
+            if string.sub(line, 1, 5):lower() == "host:" then
+                host = string.sub(line, 7)
+            end
+            line, err = client:receive()
+        end
+        if host and path then
+            print("Received request for " .. host .. path)
+        end
+        requestURL(host .. path)
+        data = getData()
+        client:send("HTTP/1.1 200 OK\r\n")
+        client:send("Server: OC2CCNetBridge\r\n")
+        client:send("Content-Type: text/html\r\n")
+        client:send("Content-Length: " .. #data .. "\r\n")
+        client:send("\r\n")
+        client:send(data)
+    end
+    client:close()
+end
---
+++ proxy.lua
@@ -0,0 +1,158 @@
+--[[
+Assume that input is in front
+output is on the bottom
+disk drive is on top
+--]]
+if not fs.exists("LibDeflate.lua") then
+    print("LibDeflate not found, downloading...")
+    shell.run("wget https://github.com/SafeteeWoW/LibDeflate/raw/main/LibDeflate.lua")
+end
+
+if not fs.exists("base64.lua") then
+    print("base64 not found, downloading...")
+    shell.run("wget https://raw.githubusercontent.com/iskolbin/lbase64/master/base64.lua")
+end
+
+local ld = require("LibDeflate")
+local base64 = require("base64")
+
+local selfName = "turtle_3"
+local diskDriveName = "drive_0"
+local ioChest = peripheral.wrap("minecraft:chest_6")
+local returnChest = peripheral.wrap("minecraft:chest_7")
+local dataChest = peripheral.wrap("minecraft:chest_8")
+local inputChest = peripheral.wrap("minecraft:chest_11")
+
+local chestSize = 27
+local statusActive = false
+
+local function getDisk()
+    local availableSlot = nil
+    while availableSlot == nil do
+        for k, _ in pairs(returnChest.list()) do
+            availableSlot = k
+            break
+        end
+        if availableSlot == nil then
+            sleep(0.05)
+        end
+    end
+    returnChest.pushItems(diskDriveName, availableSlot)
+end
+
+local function writeDataToChest(data, slot)
+    getDisk()
+    --turtle.dropUp()
+    disk.setLabel("top", data)
+    --turtle.suckUp()
+    while dataChest.list()[slot] ~= nil do
+        sleep(0.05)
+    end
+    sleep(0.05)
+    dataChest.pullItems(diskDriveName, 1, 1, slot)
+end
+
+local function setStatus(status)
+    if statusActive then
+        while dataChest.list()[1] == nil do
+            sleep(0.05)
+        end
+        dataChest.pushItems(diskDriveName, 1)
+    else
+        getDisk()
+        statusActive = true
+    end
+    --turtle.dropUp()
+    disk.setLabel("top", status)
+    --turtle.suckUp()
+    while dataChest.list()[1] ~= nil do
+        sleep(0.05)
+    end
+    sleep(0.05)
+    dataChest.pullItems(diskDriveName, 1, 1, 1)
+end
+
+local function getURL(url)
+    local req = http.get(url, { ["User-Agent"] = "OC2CCNetBridge Testing"})
+    if req then
+        local body = req.readAll()
+        req.close()
+        return body
+    else
+        return "ERROR, COULD NOT FIND PAGE " .. url
+    end
+end
+
+local function getEncoded(data)
+    local deflated = ld:CompressZlib(data)
+    local encoded = base64.encode(deflated)
+    return encoded
+end
+
+local function sendEncodedData(data)
+    local sent = 0
+    setStatus("SENDING")
+    print("status: SENDING")
+    local slot = 1
+    while sent < #data do
+        local chunk = data:sub(sent + 1, sent + 32)
+        slot = slot + 1
+        if slot > chestSize then
+            slot = 2
+        end
+        writeDataToChest(chunk, slot)
+        sent = sent + #chunk
+        print(tostring(sent) .. " of " .. tostring(#data))
+    end
+    setStatus("DONE")
+    print("Done")
+end
+
+function sendPage(url)
+    local page = getURL(url)
+
+    local encoded = getEncoded(page)
+
+    print("Got page, sending encoded data")
+
+    sendEncodedData(encoded)
+end
+
+local url = ""
+local upper = true
+local character = 0
+
+while true do
+    local inputContents = inputChest.list()
+    local data = 0
+    if inputContents[18] ~= nil then
+        for i = 1, 17 do
+            if inputContents[i] == nil then
+                data = i
+                break
+            end
+        end
+        if data == 17 then
+            inputChest.pushItems(peripheral.getName(inputChest), 18, 1, data)
+            url = "http://" .. url
+            print("Requesting " .. url)
+            sendPage(url)
+            statusActive = false
+            url = ""
+        else
+            if upper then
+                character = (data - 1) * 16
+                upper = false
+            else
+                character = character + (data - 1)
+                print("Got character " .. string.char(character))
+                url = url .. string.char(character)
+                upper = true
+            end
+            -- Move back, signalling we are ready for more
+            inputChest.pushItems(peripheral.getName(inputChest), 18, 1, data)
+        end
+    else
+        sleep(0.05)
+    end
+end
